a052085790dbbad67b79fdd4324f7608f6d3d164
cpatrick109 <casey.patrick109@gmail.com>
2016-09-20 23:00:36 -0600

    part 2 done

--- routes/books.js
+++ routes/books.js
@@ -1,10 +1,55 @@
 'use strict';
 
 const express = require('express');
-
-// eslint-disable-next-line new-cap
 const router = express.Router();
+const knex = require('../knex')
+const humps = require('humps')
 
+router.get('/', (_req, res) => {
+    knex('books').orderBy('title', 'asc').then((books) => {
+        res.send(humps.camelizeKeys(books));
+    })
+})
 
+router.get('/:id', (req, res) => {
+    knex('books').where('id', req.params.id).then((book) => {
+        res.send(humps.camelizeKeys(book[0]));
+    })
+})
+router.post('/', (req, res) => {
+    knex('books')
+        .returning(['id', 'title', 'author', 'genre', 'description', 'cover_url'])
+        .insert({
+            title: req.body.title,
+            author: req.body.author,
+            genre: req.body.genre,
+            description: req.body.description,
+            cover_url: req.body.coverUrl
+        }).then((book) => {
+            res.send(humps.camelizeKeys(book[0]));
+        });
+});
 
+router.patch('/:id', (req, res) => {
+    knex('books')
+        .where('id', req.params.id)
+        .returning(['id', 'title', 'author', 'genre', 'description', 'cover_url'])
+        .update({
+            title: req.body.title,
+            author: req.body.author,
+            genre: req.body.genre,
+            description: req.body.description,
+            cover_url: req.body.coverUrl
+        }).then((book) => {
+            res.send(humps.camelizeKeys(book[0]));
+        });
+})
+
+router.delete('/:id', (req, res) => {
+    knex('books')
+        .returning(['title', 'author', 'genre', 'description', 'cover_url'])
+        .where('id', req.params.id).del().then((book) => {
+            res.send(humps.camelizeKeys(book[0]));
+        });
+});
 module.exports = router;
